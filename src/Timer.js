b106ef35a793441f2084970c24df07d50dcc997b
heredia lucas <heredialucasfac22@hotmail.com>
2021-12-23 12:49:24 -0300

    Actualizando Tempo

--- src/Timer.js
+++ src/Timer.js
@@ -1,53 +1,50 @@
-import React, { useState, useEffect, useRef } from 'react';import "./Timer.css";
+import React, { useState, useEffect, useRef } from "react";
+import "./Timer.css";
 
 const Timer = () => {
   const [segundos, setSegundos] = useState(0);
   const [activo, setActivo] = useState(false);
   const [tipo, setTipo] = useState("Contador");
-  
+
   const myRef = useRef(null);
-  
+
   function agregaSegundos() {
-    // `current` apunta al elemento de entrada de texto montado
-    let ref = myRef.current.value
-    setSegundos(ref)
-}
+    let ref = myRef.current.value;
+    if (ref > 0) {
+      setSegundos(ref);
+    }
+  }
 
   function toggle() {
-    
-    if(myRef.current.value < 0) return alert('El número no debe ser negativo')
     setActivo(!activo);
-    myRef.current.value = 0;
   }
 
   function reset() {
-    if(tipo==="Cuenta Regresiva") myRef.current.value = 0;
     setSegundos(0);
     setActivo(false);
-    }
+  }
 
   function cambioTipo() {
     if (tipo === "Contador") setTipo("Cuenta Regresiva");
     if (tipo === "Cuenta Regresiva") setTipo("Contador");
   }
 
-
   useEffect(() => {
     let intervalo = null;
-    if (activo && tipo === 'Contador') {
+    if (activo && tipo === "Contador") {
       intervalo = setInterval(() => {
-        setSegundos(segundos => segundos + 1);
+        setSegundos((segundos) => segundos + 1);
       }, 1000);
     }
-    if (activo && tipo === 'Cuenta Regresiva') {
+    if (activo && tipo === "Cuenta Regresiva") {
       intervalo = setInterval(() => {
-        setSegundos(segundos => segundos - 1);
+        setSegundos((segundos) => segundos - 1);
       }, 1000);
     }
-    if (!activo && segundos !== 0 && tipo === 'Contador') {
+    if (!activo && segundos !== 0 && tipo === "Contador") {
       clearInterval(intervalo);
     }
-    if (segundos === 0 && tipo === 'Cuenta Regresiva') {
+    if (segundos === 0 && tipo === "Cuenta Regresiva") {
       reset();
       clearInterval(intervalo);
     }
@@ -74,7 +71,15 @@ const Timer = () => {
       <button className="button" onClick={cambioTipo}>
         {tipo}
       </button>
-      {tipo === 'Cuenta Regresiva' && <input type="number" ref={myRef} onChange={agregaSegundos} placeholder="Ingresa Segundos" autoComplete="off"/>}
+      {tipo === "Cuenta Regresiva" && (
+        <input
+          type="number"
+          ref={myRef}
+          onChange={agregaSegundos}
+          placeholder="Ingresa Segundos"
+          autoComplete="off"
+        />
+      )}
     </div>
   );
 };
